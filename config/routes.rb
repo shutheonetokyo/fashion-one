fc885e3be144497bd2bbf507890a3840392c95aa
shutheonetokyo <ss1030ss@icloud.com>
2022-01-03 00:13:08 +0900

    ホーム画面の修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
 end
