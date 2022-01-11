6ad282693bf2ee8fb35779f1396c3a0390626741
shutheonetokyo <ss1030ss@icloud.com>
2022-01-11 22:13:12 +0900

    検索結果、詳細画面の作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,10 +1,14 @@
 class ShopsController < ApplicationController
 
-  PER_PAGE = 20
+  PER_PAGE = 10
 
   def index
     @q = Shop.ransack(params[:q])
     @shops = @q.result.page(params[:page]).per(PER_PAGE)
+  end
+
+  def show
+    @shop = Shop.find(params[:id])
   end
 
 end
