be3484acb0c15f8c0362dd420bd2364395e6b70c
shutheonetokyo <ss1030ss@icloud.com>
2022-01-19 18:30:49 +0900

    関連付けとバリデーションの追加

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :shop
+  validates :user_id, uniqueness: {
+    scope: :post_id,
+    message: "は同じお店を2回以上保存済みにすることができません",
+  }
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,4 +3,7 @@ class Shop < ApplicationRecord
 
   belongs_to :area
   belongs_to :genre
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 end
