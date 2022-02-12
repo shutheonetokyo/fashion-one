f65ef27926b8b4c6b04c35dcf161e783a8034e2f
shutheonetokyo <ss1030ss@icloud.com>
2022-02-12 16:37:01 +0900

    口コミ投稿フォームの作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user! ,except: :index
-  before_action :set_review, only: %i[ show edit update destroy ]
+  before_action :set_review, only: %i[ edit update destroy ]
 
   def index
     @shop = Shop.find(params[:shop_id])
@@ -15,7 +15,7 @@ class ReviewsController < ApplicationController
   def create
     @review = current_user.reviews.new(review_params)
     if @review.save
-      redirect_to shop_reviews_path, notice: "口コミを投稿しました"
+      redirect_to shop_path(params[:shop_id]), notice: "口コミを投稿しました"
     else
       redirect_back(fallback_location: root_path, alert: "口コミの投稿に失敗しました")
     end
@@ -27,7 +27,7 @@ class ReviewsController < ApplicationController
 
   def update
     if @review.update(review_params)
-      redirect_back(fallback_location: root_path, notice: "口コミを編集しました。")      
+      redirect_to shop_path(params[:shop_id]), notice: "口コミを編集しました。" 
     else
       redirect_back(fallback_location: root_path, alert: "口コミの編集に失敗しました。")
     end
