63cca8652a6004ec69b3643f3c11d19b7c68de8f
shutheonetokyo <ss1030ss@icloud.com>
2022-02-21 16:36:29 +0900

    エラー修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,7 +4,6 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery'
-    jquery: 'jquery/src/jquery',
 }))
 
 module.exports = environment
