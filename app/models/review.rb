a9c22dd6bbdd7caa9cfbdce8e311cef5a036149a
shutheonetokyo <ss1030ss@icloud.com>
2022-02-26 21:54:04 +0900

    口コミに画像投稿機能を追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,9 @@
 class Review < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   belongs_to :user
   belongs_to :shop
-  
+
   validates :score, presence: true
   validates :title, presence: true
   validates :content, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_16_060352) do
+ActiveRecord::Schema.define(version: 2022_02_26_122252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2022_02_16_060352) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.integer "score"
+    t.string "image"
     t.index ["shop_id"], name: "index_reviews_on_shop_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
